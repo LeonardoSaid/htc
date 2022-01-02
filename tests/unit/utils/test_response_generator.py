0a88c828e28dd63d4819e8a16194be401ee7c66d
Leonardo Said <leonardocosta013@gmail.com>
2022-01-02 18:27:16 -0300

    Tests: add unittests for json response generator

--- tests/unit/utils/test_response_generator.py
+++ tests/unit/utils/test_response_generator.py
@@ -1,6 +1,9 @@
+import requests
 import unittest
 import pytest
 
+from starlette.testclient import TestClient
+from src.utils.response_generator import json_response
 
 class ResponseGeneratorTest(unittest.TestCase):
     mocker = None
@@ -9,11 +12,63 @@ class ResponseGeneratorTest(unittest.TestCase):
     def __inject_fixture(self, mocker):
         self.mocker = mocker
 
-    def setUp(self):
-        pass
+    def test_json_response_returns_empty_response(self):
+        async def app(scope, receive, send):
+            response = json_response()
+            await response(scope, receive, send)
 
-    def test_get_discount_percentage_success(self):
-        pass
+        client = TestClient(app)
+        response = client.get('/')
+        json = response.json()
 
-    def test_get_discount_percentage_raises_error(self):
-        pass
+        assert response.status_code == 200
+        assert json["messages"] == []
+        assert json["data"] == None
+
+    def test_json_response_returns_dict_data(self):
+        async def app(scope, receive, send):
+            response = json_response(
+                data={"foo": "bar"}
+            )
+            await response(scope, receive, send)
+
+        client = TestClient(app)
+        response = client.get('/')
+        json = response.json()
+
+        assert response.status_code == 200
+        assert json["messages"] == []
+        assert json["data"] == {"foo": "bar"}
+
+    def test_json_response_returns_list_data(self):
+        async def app(scope, receive, send):
+            response = json_response(
+                data=[{"foo": "bar"}]
+            )
+            await response(scope, receive, send)
+
+        client = TestClient(app)
+        response = client.get('/')
+        json = response.json()
+
+        assert response.status_code == 200
+        assert json["messages"] == []
+        assert json["data"] == [{"foo": "bar"}]
+
+    def test_json_response_returns_multiple_messages(self):
+        async def app(scope, receive, send):
+            response = json_response(
+                messages=[
+                    {"foo": "bar"},
+                    {"test": "test"}
+                ]
+            )
+            await response(scope, receive, send)
+
+        client = TestClient(app)
+        response = client.get('/')
+        json = response.json()
+
+        assert response.status_code == 200
+        assert json["messages"] == [{"foo": "bar"},{"test": "test"}]
+        assert json["data"] == None
